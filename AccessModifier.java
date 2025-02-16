41bc5e0c0c5e8bebeed47ff6d6ce87a1f4f0a7da
Abhimanyu Tomar <tomarrishabh02@gmail.com>
2025-02-16 20:27:13 +0530

    Lecture 4 completed

---
+++ AccessModifier.java
@@ -0,0 +1,7 @@
+public class AccessModifier {
+//    public : anywhere
+//    private:in same class only
+//    Default:same package
+//    Protected:Default + other package(only in subclass)
+//    these all about access modifiers.
+}
